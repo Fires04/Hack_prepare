9536c537228de25b35dd1916ab254f381552998a
David "Fires" Stein <info@davidstein.cz>
2022-12-30 14:24:30 +0100

    Update kali_prepare.sh

--- kali_prepare.sh
+++ kali_prepare.sh
@@ -8,3 +8,12 @@ chmod -R 777 /usr/share/wordlists
 
 cd ..
 rm -rf temp
+
+echo "deb http://ftp.us.debian.org/debian stretch main contrib non-free" | tee -a /etc/apt/sources.list
+
+apt-get update
+apt install chromium
+apt install filezilla
+
+mkdir Payloads
+echo "<?php echo(\"PHP active\"); phpinfo(); ?>" > ./Payloads/php_test.php
